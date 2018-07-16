a9fd6c1ac99feff96fc48d60990aaefb327894df
Linh Nguyen <linngu@w-v-kitslab-web-0.maxiv.lu.se>
2018-07-16 13:49:01 +0200

    refactored aioserver.py and change the old ws to graphql-ws

--- aioserver.py
+++ aioserver.py
@@ -10,158 +10,12 @@ the hood".
 There is also a GraphQL endpoint (/db) for querying the TANGO database.
 """
 
-from collections import defaultdict
-import json
 import logging
-import time
-from weakref import WeakSet, WeakValueDictionary
-
 import aiohttp
 from aiohttp import web
 import aiohttp_cors
 import asyncio
-from asyncio import Queue, QueueEmpty
-import PyTango
-from PyTango import (DeviceProxy, DeviceAttributeConfig, DeviceAttribute,
-                     EventType, DevFailed)
-
-from schema import tangoschema
-from listener import TaurusWebAttribute
-
-
-def serialize(events, protocol="json"):
-    "Returns event data in a serialized form according to a protocol"
-    if protocol == "json":
-        # default protocol; simplest, human readable, but also very inefficient
-        # in particular for spectrum/image data
-        return json.dumps({"events": events})
-    elif protocol == "bson":
-        # "Binary JSON" protocol. A lot more space efficient than
-        # encoding as JSON, especially for float values and arrays.
-        # There's very little size overhead.
-        # Have not looked into encoding performance.
-        return bson.dumps({"events": events})
-    raise ValueError("Unknown protocol '%s'" % protocol)
-
-
-async def consumer(keeper, ws):
-
-    """A coroutine that sends out any accumulated events once
-    per second."""
-
-    while True:
-        # check for new events once a second
-        await asyncio.sleep(1)
-        events = keeper.get()
-        # if not events:
-        #     # no events were collected
-        #     continue
-        try:
-            data = serialize(events, ws.ws_protocol)
-            if isinstance(data, bytes):
-                await ws.send_bytes(data)
-            else:
-                await ws.send_str(data)
-            logging.debug("sent %d bytes", len(data))
-        except RuntimeError as e:
-            # guess the client must be gone. Maybe there's a neater
-            # way to detect this.
-            logging.warn(e)
-            break
-    logging.info("Sender for %r exited" % ws)
-
-
-class EventKeeper:
-
-    """A simple wrapper that keeps the latest event values for
-    each attribute"""
-
-    def __init__(self):
-        self._events = defaultdict(dict)
-        self._timestamps = defaultdict(dict)
-        self._latest = defaultdict(dict)
-
-    def put(self, model, action, value):
-        "Update a model"
-        self._events[action][model] = value
-        self._timestamps[action][model] = time.time()
-
-    def get(self):
-        "Returns the latest accumulated events"
-        tmp, self._events = self._events, defaultdict(dict)
-        for event_type, events in tmp.items():
-            self._latest[event_type].update(events)
-        return tmp
-
-
-async def handle_websocket(request):
-
-    "Handles a websocket to a client over its lifetime"
-
-    ws = web.WebSocketResponse(protocols=("json", "bson"))
-    await ws.prepare(request)
-
-    logging.info("Listener has connected; protocol %s" % ws.ws_protocol)
-
-    keeper = EventKeeper()
-    loop = asyncio.get_event_loop()
-    loop.create_task(consumer(keeper, ws))
-    listeners = {}
-
-    # wait for messages over the socket
-    # A message must be JSON, in the format:
-    #   {"type": "SUBSCRIBE", "models": ["sys/tg_test/double_scalar"]}
-    # where "type" can be "SUBSCRIBE" or "UNSUBSCRIBE" and models is a list of
-    # device attributes.
-    while True:
-        msg = await ws.receive()
-        print(msg)
-        try:
-            if msg.type == aiohttp.WSMsgType.text:
-                action = json.loads(msg.data)
-                logging.debug("ws got %r", action)
-                if action["type"] == 'SUBSCRIBE':
-                    for attr in action["models"]:
-                        listener = TaurusWebAttribute(attr, keeper)
-                        listeners[attr] = listener
-                        logging.debug("add listener for '%s'", attr)
-                elif action["type"] == "UNSUBSCRIBE":
-                    for attr in action["models"]:
-                        logging.debug("remove listener for '%s'", attr)
-                        listener = listeners.pop(attr, None)
-                        if listener:
-                            listener.clear()
-            elif msg.tp == aiohttp.MsgType.error:
-                logging.warn('websocket closed with exception %s',
-                             ws.exception())
-        except RuntimeError as e:
-            logging.warn("websocket died: %s", e)
-
-    # wipe all the client's subscriptions
-    for listener in listeners.values():
-        listener.clear()
-    listeners.clear()
-
-    logging.info('websocket connection %s closed' % ws)
-
-    return ws
-
-
-async def db_handler(request):
-    "serve GraphQL queries"
-    post_data = await request.json()
-    query = post_data["query"]
-    loop = asyncio.get_event_loop()  # TODO: this looks stupid
-    try:
-        # guess we wouldn't have to do this if the client was async...
-        result = await loop.run_in_executor(
-            None, tangoschema.execute, query)
-        data = (json.dumps({"data": result.data or {}}, indent=4))
-        return web.Response(body=data.encode("utf-8"),
-                            content_type="application/json")
-    except Exception as e:
-        print(e)
-
+from routes import routes
 
 if __name__ == "__main__":
 
@@ -175,26 +29,22 @@ if __name__ == "__main__":
 		allow_headers="*",
 	    )
     })
-
-    resource = cors.add(app.router.add_resource("/db"))
-    cors.add(resource.add_route("POST", db_handler))
-
-    app.router.add_route('GET', '/socket', handle_websocket)
+    app.router.add_routes(routes)
+    for r in list(app.router.routes()):
+        cors.add(r)
     app.router.add_static('/', 'static')
-
     loop = asyncio.get_event_loop()
     handler = app.make_handler(debug=True)
-    f = loop.create_server(handler, '0.0.0.0', 5004)
-    logging.info("Point your browser to http://localhost:5004/index.html")
+    f = loop.create_server(handler, '0.0.0.0', 8000)
+    logging.info("Point your browser to http://w-v-kitslab-web-0:8000/graphiql")
     srv = loop.run_until_complete(f)
     try:
         loop.run_forever()
     except KeyboardInterrupt:
         print("Ctrl-C was pressed")
     finally:
+        loop.run_until_complete(handler.shutdown())
         srv.close()
         loop.run_until_complete(srv.wait_closed())
-        loop.run_until_complete(handler.finish_connections(1.0))
-        loop.run_until_complete(app.finish())
-
+        loop.run_until_complete(app.cleanup())
     loop.close()
---
+++ routes.py
@@ -0,0 +1,33 @@
+from aiohttp import web
+import logging
+from schema import tangoschema
+import json
+from graphql_ws.aiohttp import AiohttpSubscriptionServer
+from graphql import format_error
+
+subscription_server = AiohttpSubscriptionServer(tangoschema)
+routes = web.RouteTableDef()
+
+@routes.get('/graphiql')
+async def graphiql(request):
+    return web.FileResponse("./static/graphiql.html")
+    
+@routes.post('/db')
+async def db_handler(request):
+    "serve GraphQL queries"
+    payload = await request.json()
+    response = await tangoschema. execute(payload.get('query',''),return_promise = True)
+    data = {}
+    if response.errors:
+        data['errors'] = [format_error(e) for e in response.errors]
+    if response.data:
+        data['data'] = response.data
+    jsondata = json.dumps(data,)
+    return web.Response(text=jsondata,headers= {'Content-Type': "appication/json"})
+
+@routes.get('/socket')
+async def socket_handler(request):
+    ws = web.WebSocketResponse(protocols=('graphql-ws',))
+    await ws.prepare(request)
+    await subscription_server.handle(ws)
+    return ws
