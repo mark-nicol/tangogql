f7809f0d5bb5aa058f8ce97b4be725586b0b1f28
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-07-31 16:01:48 +0200

    Restructured graphiql, added routes

--- routes.py
+++ routes.py
@@ -8,10 +8,20 @@ from graphql import format_error
 subscription_server = AiohttpSubscriptionServer(tangoschema)
 routes = web.RouteTableDef()
 
+# httpaio doesn't support automatic serving of index files when serving directories statically,
+# so we need to define a number of routes to serve the GraphiQL interface. Is there a better way?
+
 @routes.get('/graphiql')
+async def graphiql_noslash(request):
+    return web.HTTPFound('/graphiql/')
+
+@routes.get('/graphiql/')
 async def graphiql(request):
-    return web.FileResponse("./static/graphiql.html")
-    
+    return web.FileResponse("./static/graphiql/index.html")
+
+routes.static('/graphiql/css', 'static/graphiql/css')
+routes.static('/graphiql/js', 'static/graphiql/js')
+
 @routes.post('/db')
 async def db_handler(request):
     "serve GraphQL queries"
