048bfc4303876fd42c13799a2292ecb15c6a7929
antdup <antoine.dupre@maxiv.lu.se>
2018-11-09 17:32:45 +0100

    Use tango green mode and asynchronous executors

--- tangogql/routes.py
+++ tangogql/routes.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import asyncio
 from aiohttp import web
 
 import json
 
 from graphql_ws.aiohttp import AiohttpSubscriptionServer
 from graphql import format_error
+from graphql.execution.executors.asyncio import AsyncioExecutor
 
 from tangogql.schema.tango import tangoschema
 
@@ -16,43 +18,49 @@ routes = web.RouteTableDef()
 # FIXME: aiohttp doesn't support automatic serving of index files when serving
 #        directories statically, so we need to define a number of routes to
 #        serve the GraphiQL interface. Is there a better way?
-@routes.get('/graphiql')
+@routes.get("/graphiql")
 async def graphiql_noslash(request):
-    return web.HTTPFound('/graphiql/')
+    return web.HTTPFound("/graphiql/")
 
 
-@routes.get('/graphiql/')
+@routes.get("/graphiql/")
 async def graphiql(request):
     return web.FileResponse("./static/graphiql/index.html")
 
-routes.static('/graphiql/css', 'static/graphiql/css')
-routes.static('/graphiql/js', 'static/graphiql/js')
+
+routes.static("/graphiql/css", "static/graphiql/css")
+routes.static("/graphiql/js", "static/graphiql/js")
 
 
-@routes.post('/db')
+@routes.post("/db")
 async def db_handler(request):
     """Serve GraphQL queries."""
-
+    loop = asyncio.get_event_loop()
     payload = await request.json()
-    query = payload.get('query')
-    variables = payload.get('variables')
-    response = await tangoschema.execute(query,
-                                         variable_values=variables,
-                                         return_promise=True)
+    query = payload.get("query")
+    variables = payload.get("variables")
+    # Spawn query as a coroutine using asynchronous executor
+    response = await tangoschema.execute(
+        query,
+        variable_values=variables,
+        return_promise=True,
+        executor=AsyncioExecutor(loop=loop),
+    )
     data = {}
     if response.errors:
-        data['errors'] = [format_error(e) for e in response.errors]
+        data["errors"] = [format_error(e) for e in response.errors]
     if response.data:
-        data['data'] = response.data
-    jsondata = json.dumps(data,)
+        data["data"] = response.data
+    jsondata = json.dumps(data)
 
-    return web.Response(text=jsondata,
-                        headers={'Content-Type': "application/json"})
+    return web.Response(
+        text=jsondata, headers={"Content-Type": "application/json"}
+    )
 
 
-@routes.get('/socket')
+@routes.get("/socket")
 async def socket_handler(request):
-    ws = web.WebSocketResponse(protocols=('graphql-ws',))
+    ws = web.WebSocketResponse(protocols=("graphql-ws",))
     await ws.prepare(request)
     await subscription_server.handle(ws)
     return ws
