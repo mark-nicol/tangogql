50c131f2f4cd377a8bed29972bea00048c1573c1
Linh Nguyen <linngu@w-v-kitslab-web-0.maxiv.lu.se>
2018-12-05 15:13:28 +0100

    format error

---
+++ tangogql/schema/errors.py
@@ -0,0 +1,22 @@
+""" Module containing parser for different error """
+import PyTango
+from graphql import format_error
+class ErrorParser:
+    def parse(error):
+        if isinstance(error.original_error,(PyTango.DevFailed, PyTango.ConnectionFailed,
+                PyTango.CommunicationFailed, PyTango.DeviceUnlocked)):
+            for e in error.original_error.args:
+                print("**********")
+                print (e)
+            
+            message =  {"message":[{
+                                    "desc"    : e.desc.split("\n")[0],   # only the first row contains important info
+                                    "reason"  : e.reason,
+                                    "severity": e.severity,
+                                    "origin"  : e.origin} 
+                                    for e in error.original_error.args]
+                           
+                        }
+            return message
+        else:
+            return format_error(error)
