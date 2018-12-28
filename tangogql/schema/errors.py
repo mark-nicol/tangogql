0aaf936172094ca6f99aebcfeacc50c26ab60c6f
Linh Nguyen <linngu@w-v-kitslab-web-0.maxiv.lu.se>
2018-12-28 13:00:59 +0100

    added method for removing duplicated exceptions

--- tangogql/schema/errors.py
+++ tangogql/schema/errors.py
@@ -1,18 +1,34 @@
 """ Module containing parser for different error """
 import PyTango
 from graphql import format_error
+
 class ErrorParser:
+    def filter_error(errors):
+        seen = set()
+        result_set = []
+        for message in errors:
+            duplicated = False
+            for e in message:
+                t = tuple(e.items())
+                print(t)
+                print(t not in seen)
+                if t not in seen:
+                    seen.add(t)
+                    result_set.append(message)
+        return result_set
+
+
     def parse(error):
-        if isinstance(error.original_error,(PyTango.DevFailed, PyTango.ConnectionFailed,
-                PyTango.CommunicationFailed, PyTango.DeviceUnlocked)):            
-            message =  {"message":[{
-                                    "desc"    : e.desc.split("\n")[0],   # only the first row contains important info
-                                    "reason"  : e.reason,
-                                    "severity": e.severity,
-                                    "origin"  : e.origin} 
-                                    for e in error.original_error.args]
-                           
-                        }
+        if isinstance(error.original_error,(PyTango.ConnectionFailed,PyTango.CommunicationFailed, PyTango.DevFailed)):
+            message =[]
+            for e in error.original_error.args:
+                if not e.reason == "API_CorbaException":       
+                    message.append({
+                                    "device"  : e.desc.split("\n")[0].split(" ")[-1],
+                                    "desc"    : e.desc.split("\n")[0],
+                                    "reason"  : e.reason.split("_")[-1]
+                                    })
             return message
         else:
-            return format_error(error)
+            return str(error)
+
