16693dc94e3c176de10482a1d53e2051b2b2d027
Linh Nguyen <linngu@w-v-kitslab-cc-0.maxiv.lu.se>
2019-01-09 13:57:17 +0100

    flatten list

--- tangogql/schema/errors.py
+++ tangogql/schema/errors.py
@@ -7,10 +7,8 @@ class ErrorParser:
         seen = set()
         result_set = []
         for message in errors:
-            
-            for e in message:
-                if isinstance(e,dict):
-                    t = tuple(e.items())
+                if isinstance(message,dict):
+                    t = tuple(message.items())
                 else:
                     t = tuple(e)
                 if t not in seen:
@@ -19,30 +17,28 @@ class ErrorParser:
         return result_set
 
     def parse(error):
-        message = []
-        result = {}
+        message = {}
         if isinstance(error.original_error,(PyTango.ConnectionFailed,PyTango.CommunicationFailed,PyTango.DevFailed)):
             for e in error.original_error.args:
                 if e.reason == "API_CorbaException":
                     pass
-                if e.reason == "API_CantConnectToDevice":       
-                    message.append({
-                                    "device"  : e.desc.split("\n")[0].split(" ")[-1],
-                                    "desc"    : e.desc.split("\n")[0],
-                                    "reason"  : e.reason.split("_")[-1]
-                                    })
-
-                if e.reason == "API_AttributeFailed":
+                elif e.reason == "":
+                    pass
+                elif e.reason == "API_CantConnectToDevice":       
+                    message["device"] = e.desc.split("\n")[0].split(" ")[-1]
+                    message["desc" ] = e.desc.split("\n")[0]
+                    message["reason"] = e.reason.split("_")[-1]
+                elif e.reason == "API_AttributeFailed":
                     [device,attribute] =  e.desc.split(",")      
-                    result["device"] = device.split(" ")[-1]
-                    result["attribute"] = attribute.split(" ")[-1]
-
-                if e.reason == "API_AttrValueNotSet":
-                    result["reason"] = e.reason.split("_")[-1]
-                    result["field"] = e.desc.split(" ")[1]
-            if result:
-                message.append(result)
+                    message["device"] = device.split(" ")[-1]
+                    message["attribute"] = attribute.split(" ")[-1]
+                elif e.reason == "API_AttrValueNotSet":
+                    message["reason"] = e.reason.split("_")[-1]
+                    message["field"] = e.desc.split(" ")[1]
+                else:
+                    message["reason"] = e.reason
+                    message["desc"] = e.desc
         else:
-            message.append(str(error))
+            message["desc"] = str(error)
         return message
 
