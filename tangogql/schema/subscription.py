fcd75edfeb76620681d52d410d885709412f2a23
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-02-19 11:58:13 +0100

    Made all attribute data formats work equally but increased the framerate to 1/3s

--- tangogql/schema/subscription.py
+++ tangogql/schema/subscription.py
@@ -6,7 +6,6 @@ import taurus
 import PyTango
 import numpy
 
-
 from graphene import ObjectType, String, Float, Interface, Field, List, Int
 from tangogql.schema.types import ScalarTypes
 
@@ -24,8 +23,7 @@ class AttributeFrame(ObjectType):
         return f"{self.device}/{self.attribute}"
 
 
-SLEEP_DURATION = 1.0
-GREEN_MODE = PyTango.GreenMode.Asyncio
+SLEEP_DURATION = 3.0
 
 
 class Subscription(ObjectType):
@@ -33,53 +31,45 @@ class Subscription(ObjectType):
 
     async def resolve_attributes(self, info, full_names):
         device_proxies = {}
-        attributes = []
-        ignore = []
+        name_pairs = []
 
         for full_name in full_names:
             *parts, attribute = full_name.split("/")
             device = "/".join(parts)
-            device_proxies[device] = PyTango.DeviceProxy(device, green_mode=GREEN_MODE)
-            attributes.append((device, attribute))
+            device_proxies[device] = PyTango.DeviceProxy(device)
+            name_pairs.append((device, attribute))
 
         while True:
-            for i, (device, attribute) in enumerate(attributes):
-                # Skip attributes that have been ignored due to their data format (see
-                # comment below)
-                if i in ignore:
-                    continue
-
-                try:
-                    proxy = device_proxies[device]
-                    read = await asyncio.shield(
-                        proxy.read_attribute(
-                            attribute, extract_as=PyTango.ExtractAs.List
+            try:
+                for device, attribute in name_pairs:
+                    try:
+                        proxy = device_proxies[device]
+                        read = await asyncio.shield(
+                            proxy.read_attribute(
+                                attribute, extract_as=PyTango.ExtractAs.List
+                            )
                         )
+                    except Exception:
+                        continue
+
+                    sec = read.time.tv_sec
+                    micro = read.time.tv_usec
+                    timestamp = sec + micro * 1e-6
+
+                    value = read.value
+                    write_value = read.w_value
+                    quality = read.quality.name
+
+                    yield AttributeFrame(
+                        device=device,
+                        attribute=attribute,
+                        value=value,
+                        write_value=write_value,
+                        quality=quality,
+                        timestamp=timestamp,
                     )
-                except Exception:
-                    continue
-
-                # Ignore attributes that aren't scalar after the first read, until
-                # the performance issue of continuously transmitting spectrum and
-                # image data in JSON has been addressed.
-                if read.data_format != PyTango.AttrDataFormat.SCALAR:
-                    ignore.append(i)
-
-                sec = read.time.tv_sec
-                micro = read.time.tv_usec
-                timestamp = sec + micro * 1e-6
-
-                value = read.value
-                write_value = read.w_value
-                quality = read.quality.name
-
-                yield AttributeFrame(
-                    device=device,
-                    attribute=attribute,
-                    value=value,
-                    write_value=write_value,
-                    quality=quality,
-                    timestamp=timestamp,
-                )
 
                 await asyncio.shield(asyncio.sleep(SLEEP_DURATION))
+            
+            except StopAsyncIteration:
+                break
