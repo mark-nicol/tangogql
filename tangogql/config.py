634cb1facada5d6cafb3263b09ee56acdae7cc13
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-02-28 00:14:05 +0100

    Converted tabs to spaces

--- tangogql/config.py
+++ tangogql/config.py
@@ -7,25 +7,25 @@ import json
 
 
 class Config:
-	def __init__(self, file):
-		data = json.load(file)
+    def __init__(self, file):
+        data = json.load(file)
 
-		try:
-			secret = data["secret"]
-		except KeyError:
-			raise ConfigError("no secret provided")
+        try:
+            secret = data["secret"]
+        except KeyError:
+            raise ConfigError("no secret provided")
 
-		if not isinstance(secret, str):
-			raise ConfigError("secret must be a string")
+        if not isinstance(secret, str):
+            raise ConfigError("secret must be a string")
 
-		required_groups = data.get("required_groups", [])
-		if not all(isinstance(group, str) for group in required_groups):
-			raise ConfigError("required_groups must consist of strings")
+        required_groups = data.get("required_groups", [])
+        if not all(isinstance(group, str) for group in required_groups):
+            raise ConfigError("required_groups must consist of strings")
 
-		self.secret = secret
-		self.required_groups = required_groups
+        self.secret = secret
+        self.required_groups = required_groups
 
 
 class ConfigError(Exception):
-	def __init__(self, reason):
-		super().__init__(f"Config error: {reason}")
+    def __init__(self, reason):
+        super().__init__(f"Config error: {reason}")
