946c1a33dd529fd530b3fde64703c62252f60ad5
Linh Nguyen <linngu@w-v-kitslab-web-0.maxiv.lu.se>
2019-03-20 10:20:52 +0100

    added self, info

--- tangogql/auth.py
+++ tangogql/auth.py
@@ -5,21 +5,21 @@ import logging
 logger = logging.getLogger('logger')
 
 def authorization(f):
-    def wrapper(*args, **kw):
-        config = args[1].context["config"]
+    def wrapper(self, info,*args, **kw):
+        config = info.context["config"]
         required_groups = config.required_groups
-        client = args[1].context["client"]
+        client = info.context["client"]
         memberships = client.groups
         if required_groups and not set(required_groups) & set(memberships):
             raise AuthorizationError(f"User {client.user} is not in any of the required groups")
-        return f(*args, **kw)
+        return f(self, info,*args, **kw)
     return wrapper
 
 def authentication(f):
-    def wrapper(*args, **kw):
-        if args[1].context["client"].user is None:
+    def wrapper(self, info,*args, **kw):
+        if info.context["client"].user is None:
             raise AuthenticationError("User is not authenticated")
-        return f(*args, **kw)
+        return f(self, info,*args, **kw)
     return wrapper
 
 class AuthError(GraphQLError):
