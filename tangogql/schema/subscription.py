a2d6386ec074255aaea8518b490ab0e12a5798aa
AntoineDupre <antoine.dupre@maxiv.lu.se>
2019-09-23 16:40:27 +0200

    Replace subscribtion polling with aioattribute (handle event subscribtion)

--- tangogql/schema/subscription.py
+++ tangogql/schema/subscription.py
@@ -1,13 +1,7 @@
 """Module containing the Subscription implementation."""
-
-import time
-import asyncio
-# import taurus
-import PyTango
-import numpy
-
-from graphene import ObjectType, String, Float, Field, List, Int
+from graphene import ObjectType, String, Float, Field, List
 from tangogql.schema.types import ScalarTypes
+from tangoql.base.proxies import subscriptions as subs
 
 
 class AttributeFrame(ObjectType):
@@ -30,44 +24,21 @@ class Subscription(ObjectType):
     attributes = Field(AttributeFrame, full_names=List(String, required=True))
 
     async def resolve_attributes(self, info, full_names):
-        device_proxies = {}
-        name_pairs = []
-
-        for full_name in full_names:
-            *parts, attribute = full_name.split("/")
-            device = "/".join(parts)
-            device_proxies[device] = PyTango.DeviceProxy(device)
-            name_pairs.append((device, attribute))
-
-        while True:
-            try:
-                for device, attribute in name_pairs:
-                    try:
-                        proxy = device_proxies[device]
-                        read = await proxy.read_attribute(
-                            attribute, extract_as=PyTango.ExtractAs.List
-                        )
-                    except Exception:
-                        continue
-
-                    sec = read.time.tv_sec
-                    micro = read.time.tv_usec
-                    timestamp = sec + micro * 1e-6
-
-                    value = read.value
-                    write_value = read.w_value
-                    quality = read.quality.name
-
-                    yield AttributeFrame(
-                        device=device,
-                        attribute=attribute,
-                        value=value,
-                        write_value=write_value,
-                        quality=quality,
-                        timestamp=timestamp,
-                    )
-
-                await asyncio.sleep(SLEEP_DURATION)
-
-            except StopAsyncIteration:
-                break
+        """ Setup attribute subscriibtion and return an async gen """
+        async with subs.attribute_reads(full_names) as attribute_reads:
+            async for evt in attribute_reads:
+                read = evt.read
+                sec = read.time.tv_sec
+                micro = read.time.tv_usec
+                timestamp = sec + micro * 1e-6
+                value = read.value
+                write_value = read.w_value
+                quality = read.quality.name
+                yield AttributeFrame(
+                    device=evt.device,
+                    attribute=read.name,
+                    value=value,
+                    write_value=write_value,
+                    quality=quality,
+                    timestamp=timestamp,
+                )
